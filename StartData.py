28da36cb8473597ff8cc186302cddf49135c3d03
Cesar Piangers <cesarpiangers@gmail.com>
2018-06-21 17:07:18 -0300

    StartData com tratamento caso não encontre flags, não cria a memoy layer

--- StartData.py
+++ StartData.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from qgis.core import QGis, QgsVectorLayer, QgsVectorLayer, QgsMapLayerRegistry, QgsFeature, QgsField, QgsGeometry, QGis
-from PyQt4.QtGui import QIcon, QAction
+from qgis.core import QGis, QgsVectorLayer, QgsVectorLayer, QgsMapLayerRegistry, QgsFeature, QgsField, QgsGeometry, QGis, QgsSimpleMarkerSymbolLayerV2, QgsLineSymbolV2, QgsMarkerSymbolV2, QgsMarkerLineSymbolLayerV2, QgsSimpleMarkerSymbolLayerBase, QgsSingleSymbolRendererV2
+from PyQt4.QtGui import QIcon, QAction, QColor
 from PyQt4.QtCore import QObject, SIGNAL, QVariant
 from PyQt4.QtSql import QSqlDatabase, QSqlQuery
 import resources_rc  
@@ -43,10 +43,10 @@ class StartData:
         layer = self.iface.activeLayer() 
 
         if not layer:
-            self.iface.messageBar().pushMessage("Erro", u"Esperando uma Active Layer!", level=QgsMessageBar.CRITICAL, duration=5)
+            self.iface.messageBar().pushMessage("Erro", u"Esperando uma Active Layer!", level=QgsMessageBar.CRITICAL, duration=4)
             return
         if layer.featureCount() == 0:
-            self.iface.messageBar().pushMessage("Erro", u"a camada não possui feições!", level=QgsMessageBar.CRITICAL, duration=5)
+            self.iface.messageBar().pushMessage("Erro", u"a camada não possui feições!", level=QgsMessageBar.CRITICAL, duration=4)
             return
 
         parametros = layer.source().split(" ") # recebe todos os parametros em uma lista ( senha, porta, password etc..)
@@ -86,7 +86,7 @@ class StartData:
 
         # Testa se os parametros receberam os valores pretendidos, caso não, apresenta a mensagem informando..
         if len(dbname) == 0 or len(host) == 0 or port == 0 or len(user) == 0 or len(password) == 0:
-            self.iface.messageBar().pushMessage("Erro", u'Um dos parametros não foram devidamente recebidos!', level=QgsMessageBar.CRITICAL, duration=5)
+            self.iface.messageBar().pushMessage("Erro", u'Um dos parametros não foram devidamente recebidos!', level=QgsMessageBar.CRITICAL, duration=4)
             return
 
     ####################################
@@ -103,7 +103,7 @@ class StartData:
         if not connection.isOpen(): # Testa se a conexão esta recebendo os parametros adequadamente.
             if not connection.open():
                 print 'Error connecting to database!'
-                self.iface.messageBar().pushMessage("Erro", u'Error connecting to database!', level=QgsMessageBar.CRITICAL, duration=5)
+                self.iface.messageBar().pushMessage("Erro", u'Error connecting to database!', level=QgsMessageBar.CRITICAL, duration=4)
                 print connection.lastError().text()
                 return
 
@@ -133,7 +133,6 @@ class StartData:
             self.flagsLayerProvider.addAttributes([QgsField("id", QVariant.Int), QgsField("motivo", QVariant.String)])
             self.flagsLayer.updateFields()
 
-
         self.flagsLayer.startEditing()
         ids = [feat.id() for feat in self.flagsLayer.getFeatures()]
         self.flagsLayer.deleteFeatures(ids)
@@ -149,15 +148,14 @@ class StartData:
         
         for i in source:
                 
-            if "table=" in i or "layername=" in i:
+            if "table=" in i or "layername=" in i: # Se encontrar os atributos pretendidos dentre todos do for
                 self.tableName = source[source.index(i)].split(".")[1] # Faz split em ponto e pega a segunda parte.
                 self.tableName = self.tableName.replace('"', '')
                 layerExistsInDB = True
                 break
-            
-        
+             
         if layerExistsInDB == False:
-            self.iface.messageBar().pushMessage("Erro", u"Provedor da camada corrente não provem do banco de dados!", level=QgsMessageBar.CRITICAL, duration=10)
+            self.iface.messageBar().pushMessage("Erro", u"Provedor da camada corrente não provem do banco de dados!", level=QgsMessageBar.CRITICAL, duration=4)
             return
         
         # Busca através do SQL 
@@ -190,11 +188,61 @@ class StartData:
 
         self.flagsLayerProvider.addFeatures(listaFeatures)
         self.flagsLayer.commitChanges() # Aplica as alterações à camada.
-
+        
         QgsMapLayerRegistry.instance().addMapLayer(self.flagsLayer) # Adicione a camada no mapa
+        
+        
+        ##################################
+        ##### TROCA MARCADOR DA LAYER ####
+        ##################################
+        
+        # # AQUI PRETENDO PERSONALIZAR O MARCADOR DA MEMORY LAYER, POREM A PRINCIPIO SÓ ESTA ALTERANDO A LAYER ORIGINAL.
+        # for lay in QgsMapLayerRegistry.instance().mapLayers().values():
+            
+        #     if lay == flagsLayerName and not layer.name():
 
+        #         # Estilo base.    
+        #         line = QgsLineSymbolV2()    
+
+        #         # Crie uma linha de marcadores.
+        #         marker_line = QgsMarkerLineSymbolLayerV2()
+        #         marker_line.setInterval(10)
+
+        #         # Configure o marcador.
+        #         simple_marker = QgsSimpleMarkerSymbolLayerV2()
+        #         simple_marker.setShape(QgsSimpleMarkerSymbolLayerBase.HalfSquare) # Formato
+        #         simple_marker.setSize(3)
+        #         simple_marker.setAngle(180)
+        #         simple_marker.setColor(QColor('red')) # cor
+        #         simple_marker.setOutlineColor(QColor('red')) # cor
+
+        #         # O marcador tem sua própria camada de símbolo.
+        #         marker = QgsMarkerSymbolV2()
+        #         marker.changeSymbolLayer(0, simple_marker)
+
+        #         # Adicione a camada à camada do marcador.
+        #         marker_line.setSubSymbol(marker)
+
+        #         # Finalmente, substitua a camada de símbolo no estilo base.
+        #         line.changeSymbolLayer(0, marker_line)
+
+        #         # Adicione o estilo à camada de linha.        
+        #         renderer = QgsSingleSymbolRendererV2(line)
+        #         layer.setRendererV2(renderer)
+        
+        if flagCount == 0: 
+            
+            QgsMapLayerRegistry.instance().removeMapLayer(self.flagsLayer.id())
+            self.iface.messageBar().pushMessage("Aviso", u"Não foi encontrado Flags em \"" + layer.name() + "\" !", level=QgsMessageBar.CRITICAL, duration=4)
+
+            return
         if len(query.lastError().text()) == 1:
-            self.iface.messageBar().pushMessage("Aviso", "foram geradas " + str(flagCount) + " flags para a camada \"" + layer.name() + "\" !", level=QgsMessageBar.INFO, duration=10)
+            self.iface.messageBar().pushMessage("Aviso", "foram geradas " + str(flagCount) + " flags para a camada \"" + layer.name() + "\" !", level=QgsMessageBar.INFO, duration=4)
         else:
-            self.iface.messageBar().pushMessage("Erro", u"a geração de flags falhou!", level=QgsMessageBar.CRITICAL, duration=10)
+            self.iface.messageBar().pushMessage("Erro", u"a geração de flags falhou!", level=QgsMessageBar.CRITICAL, duration=4)
             print query.lastError().text()
+
+
+
+
+
